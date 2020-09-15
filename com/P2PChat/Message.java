ffc16bb353240a6601dd24dfc4c1e7f95a6fbc28
Rahul Gaur <rgaur1620@gmail.com>
2020-09-15 18:19:24 +0530

    Add files via upload

---
+++ com/P2PChat/Message.java
@@ -0,0 +1,33 @@
+package com.P2PChat;
+
+public class Message {
+    private String msg;
+    private Sender mSender;
+
+    /**
+     * Create a new Message
+     * @param msg The String that we receive or send
+     * @param mSender Details about the person we send to
+     */
+    public Message(String msg, Sender mSender) {
+        this.msg = msg;
+        this.mSender = mSender;
+    }
+
+    public String getMessage() {
+        return msg;
+    }
+
+    public void setMessage(String m) {
+        msg = m;
+    }
+
+    public Sender getSender() {
+        return mSender;
+    }
+
+    @Override
+    public String toString() {
+        return mSender.getName() + ": " + msg;
+    }
+}
